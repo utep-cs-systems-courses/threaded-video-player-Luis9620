f32f0eba88115e1f108cb58ecf3d9c0f0a51cfe1
Luis9620 <lasotomorales@miners.utep.edu>
2020-11-08 21:18:48 -0700

    Implemented the extract frames thread

--- threadedVideoPlayer.py
+++ threadedVideoPlayer.py
@@ -4,7 +4,7 @@ import cv2, time
 
 
 class Queue:
-    def __init__(self,capacity):
+    def __init__(self, capacity):
         self.queue = []
         self.full_count = Semaphore(0)
         self.empty_count = Semaphore(24)    # 24fps
@@ -28,3 +28,30 @@ class Queue:
         return frame
 
 
+class ExtractFrames(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.videoCapture = cv2.VideoCapture('clip.mp4')    # Open the video clip
+        self.totalFrames = int(self.videoCapture.get(cv2.CAP_PROP_FRAME_COUNT))     # Total number of frames
+        self.count = 0  # Initialize number of frame counter
+
+    def run(self):
+        global frames_queue
+        # read one frame
+        success, image = self.videoCapture.read()
+
+        while True:
+            if success and len(frames_queue.queue) <= frames_queue.queueCapacity:
+                frames_queue.append_frame(image)  # Append frame to the queue
+                success, image = self.videoCapture.read()    # Read another frame
+                print(f'Reading frame {self.count}')
+                self.count += 1     # Increment the counter
+
+            if self.count == self.totalFrames:  # If counter reaches the total number of frames, break
+                frames_queue.append_frame(-1)
+                break
+        return
+
+
+if __name__ == '__main__':
+    frames_queue = Queue(9)
